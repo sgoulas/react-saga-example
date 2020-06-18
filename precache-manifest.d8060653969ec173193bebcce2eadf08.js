ed67dfd23c115922cc76c37d8f06ccb4f6b7685d
sgoulas <sprgoulas@gmail.com>
2020-06-19 00:23:02 +0300

    Updates

---
+++ precache-manifest.d8060653969ec173193bebcce2eadf08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05b82354d2f027d0d72ae09e0082fa82",
+    "url": "/react-saga-example/index.html"
+  },
+  {
+    "revision": "b62db8b5754aae9cbe4e",
+    "url": "/react-saga-example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "abe2fe9b82589b749365",
+    "url": "/react-saga-example/static/js/2.8221e8e0.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/react-saga-example/static/js/2.8221e8e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62db8b5754aae9cbe4e",
+    "url": "/react-saga-example/static/js/main.1b713330.chunk.js"
+  },
+  {
+    "revision": "a3e7b8f9e5b5fe765a83",
+    "url": "/react-saga-example/static/js/runtime-main.b9289964.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-saga-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
