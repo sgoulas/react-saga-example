c07522f4eaf2f2f330003433f8bf5f2e30383396
sgoulas <sprgoulas@gmail.com>
2020-06-20 22:55:52 +0300

    adds tests and separates fetchUser tests into a new suite

--- src/__tests__/fetchApi.test.js
+++ src/__tests__/fetchApi.test.js
@@ -1,14 +1,28 @@
 import { fetchUser } from "../store/saga.js";
 import * as fetchApi from "../fetchApi/fetchApi";
-import { call } from "redux-saga/effects";
+import { put, call } from "redux-saga/effects";
 import * as fetchActions from "../store/actions.js";
 
 describe("fetchApi tests", () => {
-  it("fetchUser should yield an Effect call(Api.fetch, './user')", () => {
-    const url = "https://jsonplaceholder.typicode.com/todos/1";
-    const action = fetchActions.sagaFetchUserInit(url);
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
+  const fetchInit = fetchActions.sagaFetchUserInit(url);
+  const iterator = fetchUser(fetchInit);
+  const response = {};
+  const error = {};
 
-    const iterator = fetchUser(action);
-    expect(iterator.next().value).toMatchObject(call(fetchApi.getUser, url));
+  describe("fetchUser suite", () => {
+    it("should yield an Effect call(Api.fetch, './user')", () => {
+      expect(iterator.next().value).toMatchObject(call(fetchApi.getUser, url));
+    });
+
+    it("should yield a fake response", () => {
+      expect(iterator.next(response).value).toMatchObject(response);
+    });
+
+    it("should yield an error effect in case of an error", () => {
+      expect(iterator.throw(error).value).toMatchObject(
+        put(fetchActions.sagaFetchUserFail())
+      );
+    });
   });
 });
