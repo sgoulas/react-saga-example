769d9a4add467aff59635bcd77431d40896b92fb
sgoulas <sprgoulas@gmail.com>
2020-06-21 05:04:01 +0300

    adds finish test, restructures suite

--- src/__tests__/fetchApi.test.js
+++ src/__tests__/fetchApi.test.js
@@ -4,27 +4,35 @@ import { fetchUser } from "../store/saga.js";
 import * as fetchApi from "../fetchApi/fetchApi";
 import * as fetchActions from "../store/actions.js";
 
-describe("fetchApi tests", () => {
+describe("fetchApi ", () => {
   const url = "https://jsonplaceholder.typicode.com/todos/1";
-  const fetchIterator = fetchUser(fetchActions.sagaFetchUserInit(url));
-  const response = {};
-  const error = {};
 
   describe("fetchUser suite", () => {
-    it("should yield an Effect call(Api.fetch, './user')", () => {
-      expect(fetchIterator.next().value).toMatchObject(
-        call(fetchApi.getUser, url)
-      );
+    const fetchIterator = fetchUser(fetchActions.sagaFetchUserInit(url));
+    const response = {};
+    describe("successfull flow", () => {
+      it("should yield an Effect call(Api.fetch, './user')", () => {
+        expect(fetchIterator.next().value).toMatchObject(
+          call(fetchApi.getUser, url)
+        );
+      });
+      it("should yield a fake response", () => {
+        expect(fetchIterator.next(response).value).toMatchObject(response);
+      });
+      it("should then finish", () => {
+        expect(fetchIterator.next().done).toBe(true);
+      });
     });
 
-    it("should yield a fake response", () => {
-      expect(fetchIterator.next(response).value).toMatchObject(response);
-    });
-
-    it("should yield an error effect in case of an error", () => {
-      expect(fetchIterator.throw(error).value).toMatchObject(
-        put(fetchActions.sagaFetchUserFail())
-      );
+    describe("error handling", () => {
+      const error = {};
+      const fetchIterator = fetchUser(fetchActions.sagaFetchUserInit(url));
+      fetchIterator.next();
+      it("should yield an error effect in case of an error", () => {
+        expect(fetchIterator.throw(error).value).toMatchObject(
+          put(fetchActions.sagaFetchUserFail())
+        );
+      });
     });
   });
 });
